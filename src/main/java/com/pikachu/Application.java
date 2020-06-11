d736c786ee594c817923525944caaf3ffd9cd129
BhalchandraNaik <bnaik2611@gmail.com>
2020-06-11 00:25:41 -0600

    removed unused import

--- src/main/java/com/pikachu/Application.java
+++ src/main/java/com/pikachu/Application.java
@@ -2,7 +2,7 @@ package com.pikachu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
