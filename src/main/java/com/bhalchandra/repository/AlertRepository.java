dc2162ea4252f713bae676c9793906aafd6b8f95
BhalchandraNaik <bnaik2611@gmail.com>
2020-06-14 23:37:41 -0600

    create method for past hours alerts

--- src/main/java/com/bhalchandra/repository/AlertRepository.java
+++ src/main/java/com/bhalchandra/repository/AlertRepository.java
@@ -1,8 +1,16 @@
 package com.bhalchandra.repository;
 
 import com.bhalchandra.entity.Alert;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
 
 public interface AlertRepository extends CrudRepository<Alert, String> {
+
+    @Query("SELECT alert FROM Alert alert WHERE alert.createdAt > :pastDate and alert.level = 'HIGH' ORDER BY alert.createdAt")
+    public List<Alert> findHighAlertsSince(@Param("pastDate") Date pastDate);
 
 }
