106c899e5d1458bfa19362dd0a4b456babcd6ca8
BhalchandraNaik <bnaik2611@gmail.com>
2020-06-14 23:36:32 -0600

    create past geoloc service

--- src/main/java/com/bhalchandra/service/ReadingServiceImpl.java
+++ src/main/java/com/bhalchandra/service/ReadingServiceImpl.java
@@ -1,15 +1,15 @@
 package com.bhalchandra.service;
 
-import com.bhalchandra.entity.Alert;
-import com.bhalchandra.entity.Reading;
-import com.bhalchandra.entity.Tires;
-import com.bhalchandra.entity.Vehicle;
+import com.bhalchandra.entity.*;
 import com.bhalchandra.repository.ReadingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -30,5 +30,28 @@ public class ReadingServiceImpl implements ReadingService {
         reading = repository.save(reading);
         alertService.checkAndCreateAlerts(reading);
         return reading;
+    }
+
+    @Override
+    public List<PastGeolocationResponse> findGeolocationWithinPastMinutes(String vehicleId, Integer pastMinutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, -pastMinutes);
+        Date date = calendar.getTime();
+
+        ArrayList<PastGeolocationResponse> results = new ArrayList<PastGeolocationResponse>();
+        List<Reading> records = repository.findGeolocationSinceMinutes(date, vehicleId);
+        records.forEach(record -> {
+            PastGeolocationResponse result = new PastGeolocationResponse();
+            result.setVehicleId(record.getVin());
+
+            GeoLocation geoLocation = new GeoLocation();
+            geoLocation.setLatitude(record.getLatitude());
+            geoLocation.setLongitude(record.getLongitude());
+
+            result.setGeoLocation(geoLocation);
+
+            results.add(result);
+        });
+        return (List<PastGeolocationResponse>) results;
     }
 }
